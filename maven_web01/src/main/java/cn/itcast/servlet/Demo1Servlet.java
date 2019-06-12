8aab19bb579588144a00d5fabf28e111d0b06b2c
chenjiang <cj@itcast.cn>
2019-06-12 20:44:02 +0800

    xiudsfd

--- maven_web01/src/main/java/cn/itcast/servlet/Demo1Servlet.java
+++ maven_web01/src/main/java/cn/itcast/servlet/Demo1Servlet.java
@@ -13,6 +13,7 @@ public class Demo1Servlet extends HttpServlet {
         request.getRequestDispatcher("/helloworld").forward(request,response);
 
         System.out.println("fdfdfdf");
+        System.out.println("有什么困难讲出来,组织上可以给你解决");
 
     }
 
